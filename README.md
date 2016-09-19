abf5bb97406fd597996747114ea9820b2a42e270
GermanBluefox <dogafox@gmail.com>
2016-09-19 21:07:02 +0200

    (bluefox) initial commit

---
+++ README.md
@@ -0,0 +1,85 @@
+![Logo](admin/pushsafer.png)
+ioBroker pushsafer Adapter
+==============
+
+[![NPM version](http://img.shields.io/npm/v/iobroker.pushsafer.svg)](https://www.npmjs.com/package/iobroker.pushsafer)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.pushsafer.svg)](https://www.npmjs.com/package/iobroker.pushsafer)
+
+[![NPM](https://nodei.co/npm/iobroker.pushsafer.png?downloads=true)](https://nodei.co/npm/iobroker.pushsafer/)
+
+
+Send pushsafer notifications from ioBroker.
+Pushsafer.com supports iOS, Android, Windows 10 (Phone & Desktop) devices and Webpush (Chrome & Firefox)
+
+## Configuration
+First of all it is required an account on pushsafer with a private key
+![Pushsafer configuration](img/Screen0.png)
+
+![Private Key](img/Screen1.png)
+
+## Usage
+
+To send notification from ScriptEngine just write: 
+
+```javascript
+// send notification to all instances of pushsafer adapter
+sendTo("pushsafer", "message body");
+
+// send notification to specific instance of pushsafer adapter
+sendTo("pushsafer.1", "message body");
+
+// To specify subject or other options
+sendTo("pushsafer", {
+   m:  'Test text',   // mandatory - your text message
+   t:    'SweetHome', // optional  - your message's title, otherwise your app's name is used
+   d:    '12',        // optional  - a device id or device group id (empty or a = all devices)
+   s:    '2',         // optional  - a number betwenn 0-28 (see pushsafers API description)
+   i:    '2',         // optional  - a number betwenn 1-98 (see pushsafers API description)
+   v:    '0'          // optional  - a number betwenn 0-3 (see pushsafers API description)
+});
+
+```
+
+## Changelog
+### 1.0.1 (2016-08-28)
+* (bluefox) filter out double messages
+
+### 1.0.0 (2016-06-01)
+* (bluefox) fix timestamp
+* (bluefox) update grunt packages
+
+### 0.1.1 (2015-05-03)
+* (bluefox) add readme link
+
+### 0.1.0 (2015-01-03)
+* (bluefox) enable npm install
+
+### 0.0.4 (2014-11-22)
+* (bluefox) support of new naming concept
+
+### 0.0.3 (2014-10-08)
+* (bluefox) add "daemon" mode to "subscribe"
+
+## License
+
+The MIT License (MIT)
+
+Copyright (c) 2014-2016 bluefox
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ pushsafer.js
@@ -0,0 +1,119 @@
+/**
+ *
+ *      ioBroker pushsafer Adapter
+ *
+ *      (c) 2014-2016 bluefox
+ *
+ *      MIT License
+ *
+ */
+
+/* jshint -W097 */// jshint strict:false
+/*jslint node: true */
+'use strict';
+var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+var Pushsafer = require('pushsafer-notifications');
+
+var adapter = utils.adapter('pushsafer');
+
+adapter.on('message', function (obj) {
+    if (obj && obj.command === 'send') processMessage(obj.message);
+    processMessages();
+});
+
+adapter.on('ready', function () {
+    main();
+});
+
+var stopTimer       = null;
+var pushsafer;
+var lastMessageTime = 0;
+var lastMessageText = '';
+
+// Terminate adapter after 30 seconds idle
+function stop() {
+    if (stopTimer) {
+        clearTimeout(stopTimer);
+    }
+
+    // Stop only if subscribe mode
+    if (adapter.common && adapter.common.mode === 'subscribe') {
+        stopTimer = setTimeout(function () {
+            stopTimer = null;
+            adapter.stop();
+        }, 30000);
+    }
+}
+
+function processMessage(message) {
+    if (!message) return;
+
+    // filter out double messages
+    var json = JSON.stringify(message);
+    if (lastMessageTime && lastMessageText === JSON.stringify(message) && new Date().getTime() - lastMessageTime < 1000) {
+        adapter.log.debug('Filter out double message [first was for ' + (new Date().getTime() - lastMessageTime) + 'ms]: ' + json);
+        return;
+    }
+    lastMessageTime = new Date().getTime();
+    lastMessageText = json;
+
+    if (stopTimer) clearTimeout(stopTimer);
+
+    sendNotification(message);
+
+    stop();
+}
+
+function processMessages() {
+    adapter.getMessage(function (err, obj) {
+        if (obj) {
+            processMessage(obj.message);
+            processMessages();
+        }
+    });
+}
+
+function main() {
+    // Adapter is started only if some one writes into "system.adapter.pushsafer.X.messagebox" new value
+    processMessages();
+    stop();
+}
+
+function sendNotification(message, callback) {
+    if (!message) message = {};
+    
+    if (!pushsafer) {
+        if (adapter.config.privatekey) {
+            pushsafer = new Pushsafer({
+                k: adapter.config.privatekey
+            });
+        } else {
+            adapter.log.error('Cannot send notification while not configured');
+        }
+    }
+
+    if (!pushsafer) return;
+
+    if (typeof message !== 'object') {
+        message = {m: message};
+    }
+
+    message.t         = message.title     || adapter.config.title;
+    message.s         = message.sound     || (adapter.config.sound ? adapter.config.sound : undefined);
+    message.i         = message.icon      || adapter.config.icon;
+    message.d         = message.device    || adapter.config.device;
+	message.v         = message.vibration || adapter.config.vibration;
+
+    adapter.log.info('Send pushsafer notification: ' + JSON.stringify(message));
+
+    pushsafer.send(message, function (err, result) {
+        if (err) {
+            adapter.log.error('Cannot send notification: ' + JSON.stringify(err));
+            if (callback) callback(err);
+            return false;
+        } else {
+            if (callback) callback();
+            return true;
+        }
+    });
+}
